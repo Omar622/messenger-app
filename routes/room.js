40a6a343ff112ffd2022d197908336dcc03d6e72
Omar622 <omaramin622@gmail.com>
2023-05-18 00:01:38 +0300

    Add routes

---
+++ routes/room.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+// get all rooms
+router.get('/', function (req, res, next) {
+  res.send('respond with a all rooms');
+});
+
+// get specific room with an id
+router.get('/:id', function (req, res, next) {
+  res.send('respond with a room ' + req.params.id + ' details');
+});
+
+// create room
+router.post('/create', function (req, res, next) {
+  res.send('to implement...');
+});
+
+// update specific room with an id
+router.put('/update/:id', function (req, res, next) {
+  res.send('to implement...');
+});
+
+// remove specific room with an id
+router.delete('/remove/:id', function (req, res, next) {
+  res.send('to implement...');
+});
+
+module.exports = router;
---
+++ routes/user.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+// get all users
+router.get('/', function (req, res, next) {
+  res.send('respond with all users');
+});
+
+// get specific user with an id
+router.get('/:id', function (req, res, next) {
+  res.send('respond with a user ' + req.params.id + ' details');
+});
+
+// create user
+router.post('/create', function (req, res, next) {
+  res.send('to implement...');
+});
+
+// update specific user with an id
+router.put('/update/:id', function (req, res, next) {
+  res.send('to implement...');
+});
+
+// remove specific user with an id
+router.delete('/remove/:id', function (req, res, next) {
+  res.send('to implement...');
+});
+
+module.exports = router;
